68d589ac593dcd2540e5d12e46dfff40ae865ccc
saifullah3396 <Saifullah3396@gmail.com>
2020-11-03 11:55:17 +0500

    Opened tiled label outputs.

--- scripts/test_facedetectir.sh
+++ scripts/test_facedetectir.sh
@@ -1,3 +1,3 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPTPATH}
-${SCRIPTPATH}/../deepstream-app/deepstream-app -c ${SCRIPTPATH}/../cfg/facedetectir/facedetectir.txt
+${SCRIPTPATH}/../deepstream-app/deepstream-app -c ${SCRIPTPATH}/../cfg/facedetectir/facedetectir.txt -t
