25dc3aa92d9e54a91e370ca9794501d7bcb02b69
saifullah3396 <saifullah3396@gmail.com>
2020-12-04 12:26:01 +0500

    Added process mode for secondary gie.

--- deepstream-app/apps-common/includes/deepstream_gie.h
+++ deepstream-app/apps-common/includes/deepstream_gie.h
@@ -33,58 +33,59 @@ extern "C"
 #include "gstnvdsinfer.h"
 #include "deepstream_config.h"
 
-typedef enum
-{
-  NV_DS_GIE_PLUGIN_INFER = 0,
-  NV_DS_GIE_PLUGIN_INFER_SERVER,
-} NvDsGiePluginType;
+    typedef enum
+    {
+        NV_DS_GIE_PLUGIN_INFER = 0,
+        NV_DS_GIE_PLUGIN_INFER_SERVER,
+    } NvDsGiePluginType;
 
-typedef struct
-{
-  gboolean enable;
+    typedef struct
+    {
+        gboolean enable;
 
-  gchar *config_file_path;
+        gchar *config_file_path;
 
-  gboolean override_colors;
+        gboolean override_colors;
 
-  gint operate_on_gie_id;
-  gboolean is_operate_on_gie_id_set;
-  gint operate_on_classes;
+        gint operate_on_gie_id;
+        gboolean is_operate_on_gie_id_set;
+        gint operate_on_classes;
 
-  gint num_operate_on_class_ids;
-  gint *list_operate_on_class_ids;
+        gint num_operate_on_class_ids;
+        gint *list_operate_on_class_ids;
 
-  gboolean have_bg_color;
-  NvOSD_ColorParams bbox_bg_color;
-  NvOSD_ColorParams bbox_border_color;
+        gboolean have_bg_color;
+        NvOSD_ColorParams bbox_bg_color;
+        NvOSD_ColorParams bbox_border_color;
 
-  GHashTable *bbox_border_color_table;
-  GHashTable *bbox_bg_color_table;
+        GHashTable *bbox_border_color_table;
+        GHashTable *bbox_bg_color_table;
 
-  guint batch_size;
-  gboolean is_batch_size_set;
+        guint batch_size;
+        gboolean is_batch_size_set;
 
-  guint interval;
-  gboolean is_interval_set;
-  guint unique_id;
-  gboolean is_unique_id_set;
-  guint gpu_id;
-  gboolean is_gpu_id_set;
-  guint nvbuf_memory_type;
-  gchar *model_engine_file_path;
+        guint interval;
+        gboolean is_interval_set;
+        guint unique_id;
+        gboolean is_unique_id_set;
+        guint gpu_id;
+        gboolean is_gpu_id_set;
+        guint nvbuf_memory_type;
+        gchar *model_engine_file_path;
 
-  gchar *label_file_path;
-  guint n_labels;
-  guint *n_label_outputs;
-  gchar ***labels;
+        gchar *label_file_path;
+        guint n_labels;
+        guint *n_label_outputs;
+        gchar ***labels;
 
-  gchar *raw_output_directory;
-  gulong file_write_frame_num;
+        gchar *raw_output_directory;
+        gulong file_write_frame_num;
 
-  gchar *tag;
+        gchar *tag;
+        gint process_mode;
 
-  NvDsGiePluginType plugin_type;
-} NvDsGieConfig;
+        NvDsGiePluginType plugin_type;
+    } NvDsGieConfig;
 
 #ifdef __cplusplus
 }
