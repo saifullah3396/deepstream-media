591a0e58114010c4aa6bdaf5c8416933c144f048
saifullah3396 <saifullah3396@gmail.com>
2020-12-04 12:45:21 +0500

    Set path from env variable.

--- scripts/craft_onnx2trt.sh
+++ scripts/craft_onnx2trt.sh
@@ -3,7 +3,7 @@ FILE="$(basename $1)"
 echo "$DIR"
 echo "$FILE"
 # in our case, craft takes input of Nx3x360x640
-/home/sai/ncai/TensorRT-7.0.0.11/bin/trtexec --fp16 --explicitBatch \
+$TENSOR_RT/bin/trtexec --fp16 --explicitBatch \
     --workspace=256 \
     --onnx="${DIR}/${FILE}" --saveEngine="${DIR}/${FILE}.trt" \
     --minShapes=\'input\':1x3x360x640 \
