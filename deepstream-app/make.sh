b969ad6a4bb97b01b5121d9809161b720b741fa4
saifullah3396 <saifullah3396@gmail.com>
2020-12-04 14:52:34 +0500

    Added script for making app.

---
+++ deepstream-app/make.sh
@@ -0,0 +1,9 @@
+#make -C ./deepstream/gst-nvinfer/ clean
+make -C ./deepstream/gst-nvinfer/
+sudo CUDA_VER=10.2 make -C ./deepstream/gst-nvinfer/ install
+
+#make -C ./deepstream/nvdsinfer/ clean
+make -C ./deepstream/nvdsinfer/
+sudo CUDA_VER=10.2 make -C ./deepstream/nvdsinfer/ install
+
+make
