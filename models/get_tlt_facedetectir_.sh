14185a291dca162a2b001d7e6a1033d891e9bd24
saifullah3396 <saifullah3396@gmail.com>
2020-12-08 18:07:01 +0500

    Moved.

--- models/get_tlt_facedetectir_.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p facedetectir && \
-    wget https://api.ngc.nvidia.com/v2/models/nvidia/tlt_facedetectir/versions/pruned_v1.0/files/resnet18_facedetectir_pruned.etlt \
-    -O facedetectir/resnet18_facedetectir_pruned.etlt && \
-    wget https://api.ngc.nvidia.com/v2/models/nvidia/tlt_facedetectir/versions/pruned_v1.0/files/facedetectir_int8.txt \
-    -O facedetectir/facedetectir_int8.txt
