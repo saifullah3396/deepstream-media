d83f7d36a8da9c031e05f5421603bb98e53ce98b
saifullah3396 <saifullah3396@gmail.com>
2020-12-08 19:46:32 +0500

    Fixed face recognition scripts.

--- scripts/face_recognition/test.sh
+++ scripts/face_recognition/test.sh
@@ -1,4 +1,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPTPATH}
 
-${SCRIPTPATH}/../media_analytics_app/media_analytics_app -c ${SCRIPTPATH}/../cfg/facedetectir/facerecognition.txt -t
+${SCRIPTPATH}/../../src/media_analytics_app/media_analytics_app -c ${SCRIPTPATH}/../../cfg/face_recognition/app_config.txt -t
+
--- scripts/face_recognition/tf2onnx.sh
+++ scripts/face_recognition/tf2onnx.sh
@@ -1,3 +1,17 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPTPATH}
-../../src/utils/tf2onnx.sh $(realpath ../../models/face_recognition/face_recognition.pb)
+
+PATH_TO_FILE=$(realpath ../../models/face_recognition/face_recognition.pb)
+
+if [[ $PATH_TO_FILE != *.pb ]]; then
+    echo 1>&2 "$0: The model must be a .pb file."
+    exit 2
+fi
+
+DIR="$(dirname $PATH_TO_FILE)"
+FILE="$(basename $PATH_TO_FILE)"
+python -m tf2onnx.convert --graphdef ${DIR}/${FILE} \
+    --opset 11 \
+    --output ${DIR}/${FILE}.onnx --inputs input_1:0 \
+    --outputs global_average_pooling2d_1/Mean:0 \
+    --inputs-as-nchw input_1:0
