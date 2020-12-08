e6a0d1b08455a6031db44252853175a3507fc43d
saifullah3396 <saifullah3396@gmail.com>
2020-12-08 19:27:58 +0500

    Fixed face detection config and scripts.

---
+++ scripts/face_detection/facedetectir/test.sh
@@ -0,0 +1,5 @@
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd ${SCRIPTPATH}
+
+${SCRIPTPATH}/../../../src/media_analytics_app/media_analytics_app -c ${SCRIPTPATH}/../../../cfg/face_detection/facedetectir/app_config.txt -t
+
