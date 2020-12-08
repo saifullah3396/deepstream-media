dcecd7b168181c5a52f8f461d5eef5e2b6bb1e3a
saifullah3396 <saifullah3396@gmail.com>
2020-12-08 18:23:39 +0500

    Fixed test.

--- scripts/text_detection/craft/test.sh
+++ scripts/text_detection/craft/test.sh
@@ -1,3 +1,3 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPTPATH}
-${SCRIPTPATH}/../deepstream-app/deepstream-app -c ${SCRIPTPATH}/../cfg/craft/craft.txt -t
+${SCRIPTPATH}/../../../src/media_analytics_app/media_analytics_app -c ${SCRIPTPATH}/../../../cfg/text_detection/craft/app_config.txt -t
