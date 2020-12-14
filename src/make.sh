21cf6944d448b35bac9453595aff3afe7363eb19
saifullah3396 <saifullah3396@gmail.com>
2020-12-14 13:13:04 +0500

    Added custom gst nvmsgconv and nvmsgconv to make.

--- src/make.sh
+++ src/make.sh
@@ -1,13 +1,23 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd ${SCRIPTPATH}
 
+# install gst-nvinfer
 # make -C ${SCRIPTPATH}/deepstream/gst-nvinfer/ clean
 make -C ${SCRIPTPATH}/deepstream_base/gst-nvinfer/
 sudo CUDA_VER=10.2 make -C ./deepstream_base/gst-nvinfer/ install
 
+# install nvdsinfer
 # make -C ${SCRIPTPATH}/deepstream/nvdsinfer/ clean
 make -C ${SCRIPTPATH}/deepstream_base/nvdsinfer/
 sudo CUDA_VER=10.2 make -C ${SCRIPTPATH}/deepstream_base/nvdsinfer/ install
+
+# install gst-nvmsgconv
+make -C ${SCRIPTPATH}/deepstream_base/gst-nvmsgconv/
+sudo CUDA_VER=10.2 make -C ${SCRIPTPATH}/deepstream_base/gst-nvmsgconv/ install
+
+# install nvmsgconv
+make -C ${SCRIPTPATH}/deepstream_base/nvmsgconv/
+sudo CUDA_VER=10.2 make -C ${SCRIPTPATH}/deepstream_base/nvmsgconv/ install
 
 make -C ${SCRIPTPATH}/nvdsinfer_parsers/text_detection/craft/
 make -C ${SCRIPTPATH}/nvdsinfer_parsers/face_recognition
