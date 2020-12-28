4b33b3f4a140222d8632315e0efa1131fc9bdc8a
saifullah3396 <Saifullah3396@gmail.com>
2020-12-28 09:35:24 +0500

    Dropped down sizes for model input by 2.

--- scripts/text_recognition/latin/torch2onnx.sh
+++ scripts/text_recognition/latin/torch2onnx.sh
@@ -7,8 +7,8 @@ FILE="$(basename $PATH_TO_FILE)"
 
 # generate 3 different models for 3 different sized inputs for text recognition
 python ../../../src/utils/torch2onnx.py --weights "${DIR}/${FILE}" \
-    --model text_recognition/latin --input_shape "[1,1,64,496]"
+    --model text_recognition/latin --input_shape "[1,1,32,248]"
 python ../../../src/utils/torch2onnx.py --weights "${DIR}/${FILE}" \
-    --model text_recognition/latin --input_shape "[1,1,64,1200]"
+    --model text_recognition/latin --input_shape "[1,1,32,600]"
 python ../../../src/utils/torch2onnx.py --weights "${DIR}/${FILE}" \
-    --model text_recognition/latin --input_shape "[1,1,64,2560]"
+    --model text_recognition/latin --input_shape "[1,1,32,1280]"
